ca0d50a3546e3a15da9f20f96f85f326089f3b31
firashedfi5 <firashedfi4@gmail.com>
2025-08-25 14:28:35 +0100

    import firebase into main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:spendwise/core/utils/app_router.dart';
 import 'package:spendwise/core/utils/service_locatore.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setup();
   runApp(const SpendWise());
 }
