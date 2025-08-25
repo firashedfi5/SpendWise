c87afe3f13ee213d2bb52c9b88d9de0ef895cd1a
firashedfi5 <firashedfi4@gmail.com>
2025-08-25 22:41:48 +0100

    fixed the login with google button

--- lib/features/auth/presentation/views/widgets/login_signup_with_google.dart
+++ lib/features/auth/presentation/views/widgets/login_signup_with_google.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:go_router/go_router.dart';
 import 'package:spendwise/core/constants.dart';
-import 'package:spendwise/core/utils/app_router.dart';
-import 'package:spendwise/core/utils/functions/custom_snackbar.dart';
 import 'package:spendwise/features/auth/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:spendwise/features/auth/presentation/manager/signup_cubit/signup_cubit.dart';
 
@@ -15,57 +12,42 @@ class LoginSignupWithGoogle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<LoginCubit, LoginState>(
-      listener: (context, state) {
-        if (state is LoginFailure) {
-          customSnackBar(
-            context: context,
-            message: state.errMessage,
-            success: false,
-          );
-        } else if (state is LoginSuccess) {
-          GoRouter.of(context).push(AppRouter.kMainScreen);
-        }
-      },
-      child: Column(
-        children: [
-          const Row(
+    return Column(
+      children: [
+        const Row(
+          children: [
+            Expanded(child: Divider(color: kPrimaryColor, thickness: 1.5)),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Text('OR'),
+            ),
+            Expanded(child: Divider(color: kPrimaryColor, thickness: 1.5)),
+          ],
+        ),
+        const SizedBox(height: 20),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            side: const BorderSide(width: 1.5, color: kPrimaryColor),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(14),
+            ),
+            fixedSize: Size(MediaQuery.of(context).size.width, 45),
+          ),
+          onPressed: () => isLogin == true
+              ? context.read<LoginCubit>().loginWithGoogle()
+              : context.read<SignupCubit>().signupWithGoogle(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(child: Divider(color: kPrimaryColor, thickness: 1.5)),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Text('OR'),
+              const FaIcon(FontAwesomeIcons.google),
+              const SizedBox(width: 10),
+              Text(
+                isLogin == true ? 'Log in with Google' : 'Sign up with Google',
               ),
-              Expanded(child: Divider(color: kPrimaryColor, thickness: 1.5)),
             ],
           ),
-          const SizedBox(height: 20),
-          OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              side: const BorderSide(width: 1.5, color: kPrimaryColor),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(14),
-              ),
-              fixedSize: Size(MediaQuery.of(context).size.width, 45),
-            ),
-            onPressed: () => isLogin == true
-                ? context.read<LoginCubit>().loginWithGoogle()
-                : context.read<SignupCubit>().signupWithGoogle(),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const FaIcon(FontAwesomeIcons.google),
-                const SizedBox(width: 10),
-                Text(
-                  isLogin == true
-                      ? 'Log in with Google'
-                      : 'Sign up with Google',
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
