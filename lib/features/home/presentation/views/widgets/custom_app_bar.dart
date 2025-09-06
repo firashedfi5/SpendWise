d461dc9ea4e04140fc3c4c9a7ffea83537ad254d
firashedfi5 <firashedfi4@gmail.com>
2025-09-06 17:08:49 +0100

    update the username after emitting the UpdateProfileSuccess state

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:spendwise/core/utils/app_router.dart';
 import 'package:spendwise/core/utils/assets.dart';
 import 'package:spendwise/core/utils/service_locator.dart';
 import 'package:spendwise/core/utils/styles.dart';
+import 'package:spendwise/features/settings/presentation/manager/update_profile_cubit/update_profile_cubit.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -13,24 +15,34 @@ class CustomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverAppBar(
       automaticallyImplyLeading: false,
-      title: Row(
-        children: [
-          const CircleAvatar(
-            backgroundColor: Colors.red,
-            backgroundImage: AssetImage(AssetsData.defaultAvatar),
-          ),
-          const SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      title: BlocBuilder<UpdateProfileCubit, UpdateProfileState>(
+        builder: (context, state) {
+          String? updatedUsername;
+          if (state is UpdateProfileSuccess) {
+            updatedUsername = state.user.displayName;
+          }
+          return Row(
             children: [
-              const Text('Welcome!', style: Styles.textStyle14),
-              Text(
-                getIt.get<FirebaseAuth>().currentUser!.displayName ?? 'Unkown',
-                style: Styles.textStyle18,
+              const CircleAvatar(
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage(AssetsData.defaultAvatar),
+              ),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Welcome!', style: Styles.textStyle14),
+                  Text(
+                    updatedUsername ??
+                        getIt.get<FirebaseAuth>().currentUser!.displayName ??
+                        'Unkown',
+                    style: Styles.textStyle18,
+                  ),
+                ],
               ),
             ],
-          ),
-        ],
+          );
+        },
       ),
       actions: [
         IconButton(
--- lib/features/settings/presentation/views/widgets/settings_header.dart
+++ lib/features/settings/presentation/views/widgets/settings_header.dart
@@ -1,27 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spendwise/core/utils/assets.dart';
 import 'package:spendwise/core/utils/service_locator.dart';
 import 'package:spendwise/core/utils/styles.dart';
+import 'package:spendwise/features/settings/presentation/manager/update_profile_cubit/update_profile_cubit.dart';
 
 class SettingsHeader extends StatelessWidget {
   const SettingsHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const CircleAvatar(
-          backgroundImage: AssetImage(AssetsData.defaultAvatar),
-          radius: 75,
-        ),
-        const SizedBox(height: 10),
-        Text(
-          getIt.get<FirebaseAuth>().currentUser!.displayName ?? 'Unkown',
-          style: Styles.textStyle22,
-        ),
-        Text(getIt.get<FirebaseAuth>().currentUser!.email ?? ''),
-      ],
+    return BlocBuilder<UpdateProfileCubit, UpdateProfileState>(
+      builder: (context, state) {
+        String? updatedUsername;
+        if (state is UpdateProfileSuccess) {
+          updatedUsername = state.user.displayName;
+        }
+        return Column(
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage(AssetsData.defaultAvatar),
+              radius: 75,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              updatedUsername ??
+                  getIt.get<FirebaseAuth>().currentUser!.displayName ??
+                  'Unkown',
+              style: Styles.textStyle22,
+            ),
+            Text(getIt.get<FirebaseAuth>().currentUser!.email ?? ''),
+          ],
+        );
+      },
     );
   }
 }
