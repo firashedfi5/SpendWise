d9c9189f6c0474fa73530d202d86f192a36822c8
firashedfi5 <firashedfi4@gmail.com>
2025-09-09 22:10:50 +0100

    updated the filtering proccess to use the transactions list from the FetchTransactionsSuccess state

--- lib/features/stats/presentation/manager/filtering_cubit/filtering_cubit.dart
+++ lib/features/stats/presentation/manager/filtering_cubit/filtering_cubit.dart
@@ -1,34 +1,42 @@
 import 'package:equatable/equatable.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:spendwise/core/utils/service_locator.dart';
-import 'package:spendwise/features/home/data/repos/home_repo.dart';
 import 'package:spendwise/features/transactions/data/models/transaction_model.dart';
 
 part 'filtering_state.dart';
 
 class FilteringCubit extends Cubit<FilteringState> {
-  FilteringCubit(this._homeRepo) : super(FilteringInitial());
+  FilteringCubit() : super(FilteringInitial());
 
-  final HomeRepo _homeRepo;
-  String type = 'Income';
+  String _type = 'Income';
+  List<TransactionModel> _allTransactions = [];
+
+  String get type => _type;
+  List<TransactionModel> get allTransactions => _allTransactions;
+
+  void setAllTransactions(List<TransactionModel> transactions) {
+    _allTransactions = transactions;
+    filterTransactions(type: _type);
+  }
+
+  void updateFilterType(String newType) {
+    _type = newType;
+    filterTransactions(type: _type);
+  }
+
+  // void removeTransaction(TransactionModel transaction) {
+  //   _allTransactions.removeWhere(
+  //     (transaction) => transaction.id == transaction.id,
+  //   );
+  //   filterTransactions(type: _type);
+  // }
 
   Future<void> filterTransactions({required String type}) async {
-    emit(FilteringLoading());
-    final result = await _homeRepo.fetchTransactions(
-      userId: getIt.get<FirebaseAuth>().currentUser!.uid,
-    );
-    result.fold(
-      (failure) => emit(FilteringFailure(errMessage: failure.message)),
-      (transactions) {
-        emit(
-          FilteringSuccess(
-            filteredTransactions: transactions
-                .where((element) => element.type == type)
-                .toList(),
-          ),
-        );
-      },
+    emit(
+      FilteringSuccess(
+        filteredTransactions: _allTransactions
+            .where((element) => element.type == type)
+            .toList(),
+      ),
     );
   }
 }
